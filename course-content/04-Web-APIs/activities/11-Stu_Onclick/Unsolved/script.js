1cdc1af55c3305aaa1f5e69e0024ad3cdd1170b7
B Tram Vu <73366610+vubao2303@users.noreply.github.com>
2021-06-03 17:23:11 -0700

    inc button

--- course-content/04-Web-APIs/activities/11-Stu_Onclick/Unsolved/script.js
+++ course-content/04-Web-APIs/activities/11-Stu_Onclick/Unsolved/script.js
@@ -1,6 +1,34 @@
-var currentCount = 
-<button onclick="decrement()">Decrement</button>
+// var currentCount = 
+// <button onclick="decrement()">Decrement</button>
 
 
-document.getElementById("decrement").addEventListener("click", currenrCount);
-document.getElementById("increment").addEventListener("click", currentCount);
+// document.getElementById("decrement").addEventListener("click", currenrCount);
+// document.getElementById("increment").addEventListener("click", currentCount);
+
+let count = 0;
+let countEl = $("#count");
+
+function setCount() {
+    console.log(countEl)
+    countEl.text(count)
+};
+
+// click A
+$("#increment").on("click", function () {
+    count++;
+    setCount();
+})
+
+// // click B
+// $("#increment").on("click", function () {
+//     count++;
+//     countEl.text(count)
+// })
+
+
+
+// const inc = document.querySelector("#increment");
+
+// inc.addEventListener("click", function () {
+//     alert: "hello"
+// });
